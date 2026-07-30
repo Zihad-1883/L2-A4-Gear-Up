7a47fdffaa6545c881a315d2c994f5140c9b70b1
Zihad-1883 <zihad.pilot18@gmail.com>
2026-07-31 01:02:47 +0600

    configure SSL in pg.Pool for Prisma pooled database

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,14 +2,16 @@ import "dotenv/config";
 import pg from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient, Prisma } from "../../prisma/src/generated/prisma/client";
+import config from "../config";
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = config.DATABASE_URL;
 const isLocalhost = connectionString?.includes("localhost") || connectionString?.includes("127.0.0.1");
 
 const pool = new pg.Pool({
   connectionString,
   ssl: isLocalhost ? false : { rejectUnauthorized: false },
 });
+
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
